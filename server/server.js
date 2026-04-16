5273e302d380e6c44d0317290992fbb97844a98f
hwykiynt <clairewouldliketosignthings@gmail.com>
2026-04-16 18:54:08 -0400

    general nj area

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.get("/api/game", (req, res) => {
     getJson({
         engine: "yelp",
         find_desc: "food",
-        find_loc: "New Brunswick, New Jersey, United States",
+        find_loc: "New Jersey, United States",
         api_key: "1caaf9f9790e745ee0ae0e72a6fd68b1db3b40c5147d1af18c64682548ab0b40"
     }, (searchJson) => {
 
